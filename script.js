74a5e456a48d1e80f4fc3056700c26f53b23a8ca
Yadnika Dabhade <yadnika178@gmail.com>
2022-10-28 09:19:33 +0530

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let loadMoreBtn = document.querySelector('#load-more');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () =>{
+   let boxes = [...document.querySelectorAll('.container .box-container .box')];
+   for (var i = currentItem; i < currentItem + 3; i++){
+      boxes[i].style.display = 'inline-block';
+   }
+   currentItem += 3;
+
+   if(currentItem >= boxes.length){
+      loadMoreBtn.style.display = 'none';
+   }
+}
 // modal js
 //grt modal element
 var modal = document.getElementById("simplemodal");
